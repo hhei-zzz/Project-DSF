f917b28323de1396cb6b5afccdb06bc454ab14f7
Leon To <suzihin03@gmail.com>
2026-03-23 05:56:40 +0100

    Jag har lagt till flera funktioner för singel-fem kort och har byggt strukturen för hur allt ska fungera.

--- big2.js
+++ big2.js
@@ -42,28 +42,98 @@ function compareBig2(a, b) {
   return suitRank(b.suit) - suitRank(a.suit);
 }
 
-//*************************//
-function CompareSingle(current, previous) {
+//Här börjar funktionerna för ettkort,par,triss,femkort
+function CheckOne(hand){
+  if (hand.length === 1){return true;}
+  else{return false;}
+}
+function CompareOne(current, previous) {
     //1. Kollar om numret är högre.
     //2. Kollar det är samma nummer och isf om "mönstret" är högre.
     //Tips: Använd or eftersom att den kommer returnera såfort den ser true.
-    return (current.number > previous.number) || (current.number === previous.number && suitRank(current.suit) > suitRank(previous.suit));
+    if (CheckOne(current) && IsOne(previous)){ //dont think i need a previous
+      return (current.number > previous.number) || (current.number === previous.number && suitRank(current.suit) > suitRank(previous.suit));
+    } 
 }
 
+function CheckPair(hand){
+  if (hand.length === 2 && hand[0].number === hand[1].number){return true;}
+  else {return false;}
+}
 function ComparePair(current, previous) {
   //1. Kollar om det är samma par(siffra)
   //2. 
+  if (CheckPair(current)){
     if (current[0].number === previous[0].number){
       const a = Math.max(suitRank(current[0].suit),suitRank(current[1].suit));
       const b = Math.max(suitRank(previous[0].suit),suitRank(previous[1].suit));
       return a>b; //Return true om a är större o false om b är större
-    }
-  
-
-    else if (a<b){
+  }
+    else if (current[0].number > previous[0].number){
+      return true;
+  }
+    else if (current[0].number < previous[0].number){
       return false;
   }
+
+  }
+  
 }
+function CheckTOAK(hand){
+  if (hand.length === 3 && hand[0].number === hand[1].number && hand[1].number === hand[2].number){return true;}
+  else {return false;}
+}
+function CompareTOAK(current, previous) {
+  //1. Kollar om det är samma par(siffra)
+  //2. 
+  if(CheckTOAK(current)){
+    return (current[0].number > previous[0].number);
+  }
+  
+}
+//Här jämför jag fem kort så jag skapar en array för "ranks" eftersom att straights kan 
+
+function CheckFive(hand){
+  if (hand.length === 5){
+    if (CheckStraightFlush(hand) || CheckFOAK(hand) || CheckFullHouse(hand) || CheckFlush(hand) || CheckStraight(hand)){
+      return true;
+    }
+  }
+  else{return false;}
+}
+function CompareFive(current, previous){
+  let currenttype = 0
+  let previoustype = 0
+  if (CheckStraight(current)){currenttype = 1;} 
+  if (CheckStraight(past)){pasttype = 1;} 
+}
+
+const BIG_TWO_RANK = {3: 1, 4: 2, 5: 3, 6: 4, 7: 5, 8: 6, 9: 7, 10: 8, 11: 9, 12: 10, 13: 11, 14: 12, 2: 13};
+const Straight = 1;
+const Flush = 2;
+const FullHouse = 3;
+const Fourofakind = 4;
+const Straightflush = 5
+const SUIT_RANK = {"DIAMONDS": 1, "CLUBS": 2, "HEARTS": 3, "SPADES": 4};
+
+function CheckStraight(hand){ //kollar endast om det är en straight
+  if (hand.length !== 5){return null;}
+  const nums = hand.map(c => c.number).sort((a,b) => a - b);
+  const numStr = nums.join(','); //för att checka om den loopar runt basically att d är en A-2-3-4-5 straight, måste göra så för vår databas är FUKEKCEN LAFKDFJ KJ DF
+  const isNormal = nums.every((n,i) => i === 0 || n === nums[i-1] + 1); //kollar om d en normal straight (ingen loop i power) 
+  const isLooped = (numStr === "2,3,4,5,14"); //hardcoded 
+  if (!isNormal && !isLooped) return null;
+  return true;
+
+  //nu måste vi hitta highcard zzzz
+  //idén är att vi ska istället för hand. nvm michael do lowk fka mig eftersom du la diamond som id1 istället för id4 :/
+  //ny idé är att jag splittar upp allt i två funnktioner och kör composition
+}
+function CompareStraight(current, previous){
+
+}
+
+
 
 
 //*************************//
