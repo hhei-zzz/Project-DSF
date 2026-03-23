a655dc1f86076c6275d9e4f82cd08dfa85043b33
Michael Truong <michaelttruong@hotmail.se>
2026-03-23 21:35:35 +0100

    ändrade på http till https

--- big2.js
+++ big2.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://13.48.46.48";
+const API_BASE = "https://13.48.46.48";
 
 const buttonPlay = document.querySelector("#play");
 const buttonPass = document.querySelector("#pass");
